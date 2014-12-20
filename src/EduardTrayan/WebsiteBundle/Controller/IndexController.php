fd309416c60e94a7041462f385553b4223758e80
unknown <unitaz_13@list.ru>
2014-12-20 21:06:13 +0200

    controller&roating

--- src/EduardTrayan/WebsiteBundle/Controller/IndexController.php
+++ src/EduardTrayan/WebsiteBundle/Controller/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace EduardTrayan\WebsiteBundle\Controller\AbstractController;
+namespace EduardTrayan\WebSiteBundle\Controller;
 
 use EduardTrayan\CommonBundle\Controller;
 
@@ -8,10 +8,10 @@ class IndexController extends AbstractController
 {
     /**
      *
-     * @return \EduardTrayan\WebsiteBundle\Controller\AbstractController\Response 
+     * @return \EduardTrayan\WebsiteBundle\Controller\AbstractController\Response
      */
    public function indexAction()
     {
         return new Response('<html><body>Hello world!</body></html>');
-    } 
-}  
+    }
+}
