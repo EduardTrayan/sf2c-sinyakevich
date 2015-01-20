614bc58fe3ebdfed1a78b3a8d4d19fce0431fd44
unknown <unitaz_13@list.ru>
2015-01-20 19:40:08 +0200

    register controller via service.yml

--- src/EduardTrayan/WebsiteBundle/Controller/IndexController.php
+++ src/EduardTrayan/WebsiteBundle/Controller/IndexController.php
@@ -3,10 +3,7 @@
 namespace EduardTrayan\WebSiteBundle\Controller\IndexController;
 use EduardTrayan\CommonBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\DependencyInjection\Definition;
-$container->setDefinition('app.index_controller', new Definition(
-    'WebSiteBundle\Controller\IndexController'
-));
+
 class IndexController extends AbstractController
 {
     /**
